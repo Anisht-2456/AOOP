a552df65d331b2dc94a1fd762f70c75c41b9681c
Anisht-2456 <2320030351@klh.edu.in>
2024-08-14 09:03:05 +0530

    Add files via upload

---
+++ Aooplab1a/src/LoggingSystem/Logger.java
@@ -0,0 +1,23 @@
+package LoggingSystem;
+
+public class Logger {
+    private static Logger single_instance = null;
+
+    private Logger() {}
+
+    public static Logger getInstance() {
+        if (single_instance == null) {
+            synchronized (Logger.class) {
+                if (single_instance == null) {
+                    single_instance = new Logger();
+                }
+            }
+        }
+        return single_instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log message: " + message);
+    }
+
+}
---
+++ Aooplab1a/src/LoggingSystem/Main.java
@@ -0,0 +1,10 @@
+package LoggingSystem;
+
+public class Main {
+	 public static void main(String[] args) {
+	        Logger logger = Logger.getInstance();
+
+	        logger.log("Application started");
+	        //logger.log("An error occurred");
+	 }
+}
